35410974345dd37caf8baed95daad99414302efb
Herry Gunawan <herryg91@gmail.com>
2018-09-02 23:09:21 +0700

    Init

--- core2/types.go
+++ core2/types.go
@@ -2,14 +2,7 @@ package core2
 
 import "sync"
 
-// type ElementProcessor interface {
-// 	// Call processes a single element. If GBK or CoGBK result, the values
-// 	// are populated. Otherwise, they're empty.
-// 	ProcessElement(ctx context.Context, elm FullValue, values ...ReStream) error
-// }
-
 type Batch interface {
-	// Do([]interface{}) error
 	Insert(interface{}) error
 	Inserts([]interface{}) error
 
@@ -17,12 +10,6 @@ type Batch interface {
 	flushWorker(int, <-chan []interface{})
 	setupFlushWorker(int)
 }
-
-// type BatchConfig struct {
-// 	maxData   int
-// 	mutex     *sync.RWMutex
-// 	fHandlers BufferHandlerFunc
-// }
 
 type FlushConfig struct {
 	MaxData int
@@ -40,19 +27,5 @@ type MemoryBatch struct {
 	// jobs        chan []interface{}
 
 }
-
-// type (
-// 	Instance struct {
-// 		MaxData   int
-// 		mutex     *sync.RWMutex
-// 		datas     []interface{}
-// 		fHandlers BufferHandlerFunc
-
-// 		totalWorker int
-// 		jobs        chan []interface{}
-
-// 		batchType batchtype
-// 	}
-// )
 
 type BufferHandlerFunc func([]interface{}) error
